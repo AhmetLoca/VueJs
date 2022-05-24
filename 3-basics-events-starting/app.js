9713abcd84a8ce691ffac7b0a0bd9dc3c57888ae
ahmetloca <locaahmet@gmail.com>
2022-05-24 17:52:20 +0300

    exploring event modifiers

--- 3-basics-events-starting/app.js
+++ 3-basics-events-starting/app.js
@@ -1,9 +1,32 @@
+
+
 const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name : '',
+      confirmedName : ''
     };
   },
+  methods : {
+      confirmInput(){
+        this.confirmedName = this.name;
+      },
+      submitForm(event){
+        alert('Submitted!');
+      },
+      setName(event, lastName){
+        this.name = event.target.value + ' ' + lastName; //vanillaJS
+      },
+      add(num){
+      this.counter = this.counter + num;
+      // this.counter++;    
+    },
+    remove(){
+      this.counter--;
+      //this.counter = this.counter -1;
+    }
+  }
 });
 
 app.mount('#events');
