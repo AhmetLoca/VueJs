3689b5a02cf5cb5300072014e6d722a4c44ff89a
ahmetloca <locaahmet@gmail.com>
2022-05-27 16:32:10 +0300

    Heal functionality

--- 11-prj-monster-01-starting-setup/app.js
+++ 11-prj-monster-01-starting-setup/app.js
@@ -2,11 +2,14 @@ function getRandomValue(min,max){
   return Math.floor(Math.random() * (max - min) + min);
 }
 
+let currentRound = 0;
+
 const app = Vue.createApp({
   data(){
     return{
       playerHealth: 100,
-      monsterHealth: 100
+      monsterHealth: 100,
+      currentRound : 0
     };
   },
   computed: {
@@ -15,16 +18,26 @@ const app = Vue.createApp({
     },
     playerBarStyles(){
       return {width: this.playerHealth + '%'};
-    }
+    },
+    mayUseSpecialAttack(){
+      return this.currentRound % 3 !== 0;
+    },
+    specialAttackMonster(){
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth = this.monsterHealth - attackValue;
+      this.attackPlayer();
+    },
   },
   methods : {
     attackMonster(){
-    const attackValue = getRandomValue(5,12);
+    this.currentRound++;
+    const attackValue = getRandomValue(5, 12);
     this.monsterHealth = this.monsterHealth - attackValue;  
     this.attackPlayer();
     },
     attackPlayer(){
-      const attackValue = getRandomValue(8,15);
+      const attackValue = getRandomValue(8, 15);
       this.playerHealth = this.playerHealth - attackValue; 
       }
   }
